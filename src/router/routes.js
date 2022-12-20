527674d35c231d41655a51a61adc3db73638d550
Sergio Da Silva <sdasilva@tlmnetwork.local>
2022-12-20 11:44:53 -0400

    Agregadas rutas de aplicación

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,19 +1,28 @@
-
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
+      { path: "", component: () => import("pages/IndexPage.vue") },
+      { path: "/mostrar", component: () => import("pages/MostrarInfo.vue") },
+      { path: "/agregar", component: () => import("pages/AgregarInfo.vue") },
+      { path: "/eliminar", component: () => import("pages/EliminarInfo.vue") },
+      {
+        path: "/modificar",
+        component: () => import("pages/ModificarInfo.vue"),
+      },
+      { path: "/graficos", component: () => import("pages/GraficarInfo.vue") },
+      { path: "/about", component: () => import("pages/IndexPage.vue") },
+      { path: "/login", component: () => import("pages/IndexPage.vue") },
+    ],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
-]
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/ErrorNotFound.vue"),
+  },
+];
 
-export default routes
+export default routes;
