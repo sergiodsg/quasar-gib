f765133bd32e86543fa4d93c26fc7a2c0c61762a
Sergio Da Silva <sdasilva@tlmnetwork.local>
2022-12-20 13:34:05 -0400

    Agregado componete de tabla para la vista 'Mostrar'

---
+++ src/components/TableProducts.vue
@@ -0,0 +1,26 @@
+<script setup>
+const columns = [
+  {
+    name: 'aba',
+    required: true,
+    label: 'Aba',
+    align: 'left',
+    field: row => row.name,
+    format: val => `${val}`,
+    sortable: true
+  },
+  { name: 'nombre_banco', align: 'center', label: 'Nombre de banco', field: 'nombre_banco', sortable: true },
+  { name: 'ambiente', label: 'Ambiente', field: 'ambiente', sortable: true },
+  { name: 'switch', label: 'Switch', field: 'switch' },
+  { name: 'producto', label: 'Producto', field: 'producto' },
+  { name: 'puerto', label: 'Puerto', field: 'puerto' },
+  { name: 'direccion_ip_banco', label: 'Dirección IP de Banco', field: 'direccion_ip_banco', sortable: true },
+  { name: 'direccion_ip_empresa', label: 'Dirección IP de Empresa', field: 'direccion_ip_empresa', sortable: true }
+]
+</script>
+
+<template>
+  <div class="q-pa-md">
+    <q-table title="Productos" :rows="rows" :columns="columns" row-key="name" dense="true" />
+  </div>
+</template>
--- src/pages/MostrarInfo.vue
+++ src/pages/MostrarInfo.vue
@@ -1,9 +1,9 @@
 <script setup>
-
+import TableProducts from 'src/components/TableProducts.vue';
 </script>
 
 <template>
   <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-vertical.svg" style="width: 200px; height: 200px">
+    <TableProducts />
   </q-page>
 </template>
