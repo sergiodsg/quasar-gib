d5fa93171bb13a917bb3535231568b69d13b0e4b
Sergio Da Silva <sdasilva@tlmnetwork.local>
2022-12-21 10:16:19 -0400

    Agregada vista para modificar productos

---
+++ src/components/FormModifyProducts.vue
@@ -0,0 +1,90 @@
+<script setup>
+import { useQuasar } from 'quasar';
+import { ref } from 'vue';
+
+const $q = useQuasar();
+
+const abaMod = ref(null);
+const puertoMod = ref(null);
+const aba = ref(null);
+const nombreBanco = ref(null);
+const ambiente = ref(null);
+const switchProducto = ref(null);
+const producto = ref(null);
+const puerto = ref(null);
+const direccion_ip_banco = ref(null);
+const direccion_ip_empresa = ref(null);
+
+const onSubmit = () => {
+  $q.notify({
+    color: 'green-4',
+    textColor: 'white',
+    icon: 'cloud_done',
+    message: 'Submitted'
+  })
+}
+
+const onReset = () => {
+  abaMod.value = null;
+  puertoMod.value = null;
+  aba.value = null;
+  nombreBanco.value = null;
+  ambiente.value = null;
+  switchProducto.value = null;
+  producto.value = null;
+  puerto.value = null;
+  direccion_ip_banco.value = null;
+  direccion_ip_empresa.value = null;
+}
+</script>
+
+<template>
+  <div class="q-pa-md" style="max-width: 1000px">
+    <h5>Modificar producto existente</h5>
+    <q-form @submit="onSubmit" @reset="onReset">
+      <p>Aba y puerto del producto a modificar</p>
+      <div class="row col-4 col-sm-4 col-md-6 q-gutter-sm justify-center">
+        <q-input filled v-model="abaMod" label="Aba del producto a modificar" lazy-rules
+          :rules="[val => val && val.length > 0 || 'Campo vacío']" />
+
+        <q-input filled v-model="puertoMod" label="Puerto del producto a modificar" lazy-rules
+          :rules="[val => val && val.length > 0 || 'Campo vacío']" />
+      </div>
+
+      <p>Nueva información</p>
+      <div class="row col-4 col-sm-4 col-md-6 q-gutter-sm justify-center">
+        <q-input filled v-model="aba" label="Nuevo Aba" lazy-rules
+          :rules="[val => val && val.length > 0 || 'Campo vacío']" />
+
+        <q-input filled v-model="nombreBanco" label="Nuevo nombre del banco" lazy-rules
+          :rules="[val => val && val.length > 0 || 'Campo vacío']" />
+
+        <q-input filled v-model="ambiente" label="Nuevo ambiente" lazy-rules
+          :rules="[val => val && val.length > 0 || 'Campo vacío']" />
+
+        <q-input filled v-model="switchProducto" label="Nuevo switch" lazy-rules
+          :rules="[val => val && val.length > 0 || 'Campo vacío']" />
+      </div>
+
+      <div class="row col-4 col-sm-4 col-md-6 q-gutter-sm justify-center">
+        <q-input filled v-model="producto" label="Nuevo producto" lazy-rules
+          :rules="[val => val && val.length > 0 || 'Campo vacío']" />
+
+        <q-input filled v-model="puerto" label="Nuevo puerto" lazy-rules
+          :rules="[val => val && val.length > 0 || 'Campo vacío']" />
+
+        <q-input filled v-model="direccion_ip_banco" label="Nueva dirección IP del banco" lazy-rules
+          :rules="[val => val && val.length > 0 || 'Campo vacío']" />
+
+        <q-input filled v-model="direccion_ip_empresa" label="Nueva dirección IP de la empresa" lazy-rules
+          :rules="[val => val && val.length > 0 || 'Campo vacío']" />
+      </div>
+
+      <div class="row q-gutter-sm justify-center">
+        <q-btn label="Modificar" type="submit" color="primary" />
+        <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+      </div>
+    </q-form>
+
+  </div>
+</template>
--- src/pages/ModificarInfo.vue
+++ src/pages/ModificarInfo.vue
@@ -1,9 +1,9 @@
 <script setup>
-
+import FormModifyProducts from 'src/components/FormModifyProducts.vue';
 </script>
 
 <template>
-  <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-vertical.svg" style="width: 200px; height: 200px">
+  <q-page class="flex justify-center">
+    <FormModifyProducts />
   </q-page>
 </template>
